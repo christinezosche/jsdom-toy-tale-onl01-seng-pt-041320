ab545009e4f1d0fadfd1b1445b3ee1aaf3ac2555
christinezosche <christine.zosche@gmail.com>
2020-11-03 10:32:26 -0800

    commit tree 6a5e4f47365eab22c2dca9815b415185deecdaeb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  fetch("http://localhost:3000/toys")
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(objects) {
+              renderToys(objects);
+            });
+
+});
+
+function renderToys(toys) {
+  const collection = document.getElementById('toy-collection')
+  toys.forEach(toy => {
+    const newDiv = document.createElement('div')
+    const name = document.createElement('h2')
+    name.innerHTML = toy.name
+    const image = document.createElement('img')
+    image.innerHTML = toy.image
+    image.setAttribute('class', 'toy-avatar')
+    const likes = document.createElement('p')
+    likes.innerHTML = ``${toy.likes} Likes"
+    const button = document.createElement('button')
+    button.innerHTML = ``Like <3"
+    button.setAttribute('class', 'like-btn')
+    newDiv.appendChild(name, image, likes, button)
+    newDiv.setAttribute('class', 'card')
+    collection.appendChild(newDiv)
+  })
+}
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
